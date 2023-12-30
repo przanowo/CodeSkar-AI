34aed9a779d9c23ef0b74ffe70de608bc80a687a
przanowo <oskar.przanowski@gmail.com>
2023-12-30 22:18:47 +0100

    2

--- server/server.js
+++ server/server.js
@@ -25,9 +25,9 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt
 
-    const response = await openai.createCompletion({
+    const response = await openai.createChatCompletion({
       model: 'gpt-4',
-      prompt: `${prompt}`,
+      messages: [{ role: 'user', content: `${prompt}` }],
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
